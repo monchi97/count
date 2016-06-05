e7f372fb61bd12ba084345f97a4c1198210111a1
Shimon Nishimura <s_mina_64@yahoo.co.jp>
2016-06-05 14:43:31 +0900

    初めてのコミット

--- County/ViewController.swift
+++ County/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var number : Int = 0
+    @IBOutlet var label:UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+    }
 
 }
 
